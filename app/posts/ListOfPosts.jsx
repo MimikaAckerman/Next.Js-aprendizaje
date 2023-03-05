bb700cbe12831d85facdfaa119a495fd3458ecec
Mimika Ackerman <emily.herreragomez@gmail.com>
2023-03-05 18:35:55 +0100

    aprendido build

--- app/posts/ListOfPosts.jsx
+++ app/posts/ListOfPosts.jsx
@@ -1,3 +1,5 @@
+import {LikeButton} from "./LikeButton";
+
 const fetchPosts = () => {
   return fetch("https://jsonplaceholder.typicode.com/posts").then((response) =>
     response.json()
@@ -7,10 +9,13 @@ const fetchPosts = () => {
 export default async function ListOfPosts() {
   const posts = await fetchPosts();
 
+
+
   return posts.map((post) => (
     <article key={post.id}>
       <h2 style={{color:"yellow"}}>{post.title}</h2>
       <p>{post.body}</p>
+      <LikeButton id={post.id}/>
     </article>
   ));
 }
